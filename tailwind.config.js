880326ae7044cdd82a0a968c474d44c8f87df0fd
Alitaqi <aalitaqi349@gmail.com>
2024-08-25 05:00:03 +0500

    last commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html'],
+  content: ["./build/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        'widescreen': {'raw': '(min-aspect-ratio: 3/2)'},
+        'tallscreen': {'raw': '(max-aspect-ratio: 13/20)'},
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': {transform: 'scaleY(0)'},
+          '80%': {transform: 'scaleY(1.2)'},
+          '100%': {transform: 'scaleY(1)'}
+      },
+    },
+      animation : {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
+      }
+    },
   },
   plugins: [],
 }
-
